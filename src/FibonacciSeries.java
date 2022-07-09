4541c7727e2f31faec6e1fee977d13e4bce7d955
S <jyothsna.s@capgemini.com>
2022-07-09 10:19:44 +0530

    Initial commit

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,16 @@
+
+public class FibonacciSeries {
+	public static void main (String[] args) {
+	int targetedNumber=10;
+	int a=0,b=1,temp=0;
+	System.out.print(a+" "+b+" ");
+	for(int i=2;i<targetedNumber;i++) {
+		temp=a+b;
+		System.out.print(temp+" ");
+		a=b;
+		b=temp;
+		
+	}
+
+}
+}
