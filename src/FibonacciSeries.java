76903d2c008808b8435caabeb517356dacb461f4
Jyothsna S <jyothsnaseeni@gmail.com>
2022-07-09 10:35:20 +0530

    variable declaration changed

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -2,7 +2,9 @@
 public class FibonacciSeries {
 	public static void main (String[] args) {
 	int targetedNumber=10;
-	int a=0,b=1,temp=0;
+	int a=0;
+        int b=1;
+        int temp=0;
 	System.out.print(a+" "+b+" ");
 	for(int i=2;i<targetedNumber;i++) {
 		temp=a+b;
